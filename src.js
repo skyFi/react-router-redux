3fda55e79cab454b7e22fc3373863e870ee8b485
方永乐 <fangyongle@innobuddy.com>
2017-11-22 18:26:48 +0800

    babel

--- src.js
+++ src.js
@@ -1,8 +1,19 @@
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom'
+'use strict';
 
-export default  (...connectParams) => {
-  return (component) => {
-    return withRouter(connect(connectParams)(component));
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _reactRedux = require('react-redux');
+
+var _reactRouterDom = require('react-router-dom');
+
+exports.default = function () {
+  for (var _len = arguments.length, connectParams = Array(_len), _key = 0; _key < _len; _key++) {
+    connectParams[_key] = arguments[_key];
   }
+
+  return function (component) {
+    return (0, _reactRouterDom.withRouter)((0, _reactRedux.connect)(connectParams)(component));
+  };
 };
