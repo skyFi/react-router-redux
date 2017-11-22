32896a18ccaf0e4d2a0e24208dd83c2deabce07f
方永乐 <fangyongle@innobuddy.com>
2017-11-22 18:36:16 +0800

    use es5

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 'use strict';
 
-require('./src');
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _reactRedux = require('react-redux');
+
+var _reactRouterDom = require('react-router-dom');
+
+exports.default = function () {
+  var _args = arguments;
+  return function (component) {
+    return (0, _reactRouterDom.withRouter)((0, _reactRedux.connect)(_args[0], _args[1], _args[2], _args[3])(component));
+  };
+};
